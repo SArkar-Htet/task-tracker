750f3a265c700428ce644266ca62c11b43644cce
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2021-11-23 21:05:29 +0630

    feat: add header components

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="btn" :style="{backgroundColor: color,}">{{ text }}</button>
+</template>
+
+<script>
+  export default{
+    name: "Button",
+    props: {
+      color: String,
+      text: String,
+    }
+  }
+</script>
